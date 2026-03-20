3711fb6a4ad9db83cce7acf428bb889a6ccb1e0b
GrahamCT <70510121+GrahamCT@users.noreply.github.com>
2026-03-20 14:51:26 +0200

    debug step 1

--- app/main.py
+++ app/main.py
@@ -1,26 +1,7 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
-from app.routers import testing
-from app.routers import crm 
-from app.routers import booking
-from app.routers import establishment
-from app.routers import admin
-from app.routers import tug
+app = FastAPI()
 
-
-app = FastAPI(title="GoRhino")
-
-#ROUTERS
-app.include_router(testing.router)
-app.include_router(crm.router)
-app.include_router(booking.router)
-app.include_router(establishment.router)
-app.include_router(admin.router)
-app.include_router(tug.router)
-
-app.mount(
-    "/static",
-    StaticFiles(directory="app/static"),
-    name="static",
-)
+@app.get("/")
+async def root():
+    return {"ok": True}
